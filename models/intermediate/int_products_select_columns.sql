a4237f2e8eac4762e67e4bf338936c6c80b663e8
rafaelMachado1891 <rafadmn1@gmail.com>
2025-01-09 22:42:47 -0300

    update projeto

--- models/intermediate/int_products_select_columns.sql
+++
@@ -1,9 +0,0 @@
-with int_products_select_columns as (
-    select
-     product_id,
-     product_name,
-     category_id
-    from {{ ref("stg_products") }}
-)
-
-select * from int_products_select_columns
