3795c6e5f81a14b7e89eed34096353297566f883
rafaelMachado1891 <rafadmn1@gmail.com>
2025-06-19 19:35:28 -0300

    camada staging

---
+++ projeto_dbt_airflow/2_dw/dw_dbt/models/staging/stg_cadastros.sql
@@ -0,0 +1,28 @@
+with source as (
+    select * from {{ ref('cadastros') }}
+),
+
+transformado as (
+    select
+        -- Chaves
+        id as id_cliente,
+        cpf,
+        
+        -- Dados pessoais
+        nome,
+                
+        -- Endereço
+        cidade,
+        estado,
+        pais,
+        
+        -- Datas
+        data_cadastro as dt_cadastro,
+        
+        -- Metadados
+        current_timestamp as etl_inserted_at
+        
+    from source
+)
+
+select * from transformado
